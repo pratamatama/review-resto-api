bbbbad7b8f28a9d041e45cdc8f2c82b2fc762a76
Donny Pratama <donnypratama1024@gmail.com>
2023-02-08 13:46:56 +0700

    feat(seeder): add test user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if (config('app.debug', false)) {
+            User::create(['name' => 'tester', 'email' => 'someone@email.com', 'password' => bcrypt('password')]);
+        }
+    }
+}
